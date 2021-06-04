07da403cfd73555bad4bdc44c6d65ff14c2fb876
aneeshBhat <aneeshbhat92@gmail.com>
2021-06-04 18:24:57 +0530

    new comp portfilio added

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, HostListener, ElementRef, OnInit } from '@angular/core';
+import Typed from 'typed.js';
+import { BehaviorSubject } from 'rxjs';
+// import Waypoint from 'waypoints'
+declare const Waypoint: any;
+
 
 @Component({
   selector: 'app-portfolio',
@@ -6,10 +12,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  currentPos: BehaviorSubject<any|Number> = new BehaviorSubject(0);
+  title = 'shashiPortfoilio';
+  options = {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 40
+  };
+  @HostListener('window:scroll', ['$event']) 
+  onScroll(e:Event){
+   
+    let query= document.querySelector('.back-to-top');
+    if(Math.round(e['path'][1].scrollY) > 100){
+      query.classList.add('active')
+    }else{
+      query.classList.remove('active')
+    }
+  }
 
-  constructor() { }
+  
+
+  getYPosition(e: Event): number {
+    return (e.target as Element).scrollTop;
+  }
+
+   select = (el, all = false):any => {
+    el = el.trim()
+    if (all) {
+      return [{...document.querySelectorAll<any>(el)}]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  keepTrack(){
+    let scrol = this.currentPos.next(window.scrollY);
+    console.log(scroll,'scroll');
+  }
+  skills(){
+    let skilsContent =document.querySelector('.skills-content');
+    if (skilsContent) {
+      new Waypoint({
+        element: skilsContent,
+        offset: '80%',
+        handler: function(direction) {
+          let progress = document.querySelectorAll('.progress .progress-bar');
+          progress.forEach((el) => {
+            el['style'].width = el.getAttribute('aria-valuenow') + '%'
+          });
+        }
+      })
+    }
+    }
+
+  constructor() { 
+    this.keepTrack();
+  }
 
   ngOnInit(): void {
+    const options = {
+      strings: ['Freelancer.', 'artist.'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+ };
+ 
+ const typed = new Typed('.typed', options);
+ console.log(window.scrollY)
+ let backtotop = document.querySelectorAll('.back-to-top');
+ console.log(backtotop);
+ this.skills();
   }
+
+  MobileNav(){
+    document.querySelector('body').classList.toggle('mobile-nav-active')
+     console.log(document.querySelector('body').classList)
+  }
+
 
 }
